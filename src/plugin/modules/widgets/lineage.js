cc546111ae3e303cb126d60860a8add0c231b40c
Dan Gunter <dkgunter@lbl.gov>
2016-02-05 10:49:44 -0800

    added return null

--- src/plugin/modules/widgets/lineage.js
+++ src/plugin/modules/widgets/lineage.js
@@ -128,7 +128,7 @@ define([
                                     ;
                                 }
                                 
-                                return;
+                                return null;
                             })
                         }
                         
