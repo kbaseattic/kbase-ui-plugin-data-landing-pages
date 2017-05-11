638789589fc3d1641ed600d009aea8e47c8fceed
Jim Thomason <jim@japanesewartuba.com>
2017-05-11 11:29:29 -0500

    just commented out the statistics graphs

--- src/plugin/modules/widgets/assembly.js
+++ src/plugin/modules/widgets/assembly.js
@@ -19,12 +19,13 @@ define([
             var parent, container, runtime = config.runtime,
                 div = html.tag('div'),
                 templates = {
-                    overview: 
+                    overview:
                           '<div class=\'row\'>'
                         + '    <div class=\'col-md-12 overview-content\'>'
                         + '    </div>'
                         + '</div>',
-                    quality: '<div class=\'row\'>'
+                    // XXX - commenting out the Assembly Statistics, just in case we want to put 'em back in quickly
+                    /*quality: '<div class=\'row\'>'
                            + '    <div class=\'col-md-5 col-md-offset-1\'>'
                            + '        <div data-element=\'contig_gc_hist\'></div>'
                            + '    </div>'
@@ -39,7 +40,7 @@ define([
                            + '    <div class=\'col-md-5\'>'
                            + '        <div data-element=\'nx_plot\'></div>'
                            + '    </div>'
-                           + '</div>',
+                           + '</div>',*/
                     annotations: '<div class=\'row\'>'
                                + '    <div data-element=\'linked_annotations\'></div>'
                                + '</div>',
@@ -48,7 +49,7 @@ define([
                           + '</div>'
                 };
 
-                
+
             // VIEW
 
             function layout() {
@@ -57,10 +58,11 @@ define([
                         title: 'Assembly Summary',
                         content: div({id: 'overview'}, html.loading())
                     }),
-                    html.makePanel({
+                    // XXX - commenting out the Assembly Statistics, just in case we want to put 'em back in quickly
+                    /*html.makePanel({
                         title: 'Assembly Statistics',
                         content: div({id: 'quality'}, html.loading())
-                    })                    
+                    })*/
                 ]);
             }
 
@@ -213,7 +215,7 @@ define([
                     plotly.newPlot(o.div, o.data, o.layout);
                 });
             }
-            
+
 
             // WIDGET API
 
@@ -232,14 +234,15 @@ define([
                  *   objectVersion
                  *   ...
                  */
-                
+
                 container.querySelector('div[id="overview"]').innerHTML = templates.overview;
-                container.querySelector('div[id="quality"]').innerHTML = templates.quality;
- 
+                // XXX - commenting out the Assembly Statistics, just in case we want to put 'em back in quickly
+                //container.querySelector('div[id="quality"]').innerHTML = templates.quality;
+
                 Array.from(container.querySelectorAll('[data-element]')).forEach(function (e) {
                     e.innerHTML = html.loading();
                 });
-                
+
                 // get the assembly reference
                 var assemblyRef = utils.getRef(params);
 
@@ -301,10 +304,10 @@ define([
 
             function detach() {
                 // nothing to do necessarily, since the parent dom node will
-                // be removed the controller for this widget removes it, 
+                // be removed the controller for this widget removes it,
                 // but it is nice to take responsibility for undoing what we
                 // changed in the parent node:
-                
+
                 if (parent && container) {
                     container.innerHTML = '';
                     parent.removeChild(container);
